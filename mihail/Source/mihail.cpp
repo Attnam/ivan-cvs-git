301409438441c0242a14501b2b2e45e1c6937561
hejosa <first.last@tut.fi>
2003-04-07 18:03:35 +0300

    Mihail added

---
+++ mihail/Source/mihail.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <fstream>
+
+#define HIGH_SCORE_FILENAME ""
+#include "hscore.h"
+
+int Main(int argc, char** argv)
+{
+  if(argc != 4)
+    {
+      std::cout << "\nUsage: mihail [1. input] [2. input] [output]" << std::endl;
+      return 1;
+    }
+  highscore Input(argv[1]);
+  highscore Output(argv[2]);
+  Input.AddToFile(&Output);
+  Output.Save(argv[3]);
+  return 0;
+}
+
